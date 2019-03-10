88cce8b2f8cf7d02604e70dec89c66a83871218c
damsarar <damsarar@gmail.com>
2019-03-10 22:43:23 +0530

    Second commit

---
+++ src/question12.scala
@@ -0,0 +1,15 @@
+import scala.math.sqrt
+import scala.math.pow
+
+object question12 {
+  def main(args : Array[String]){
+    var u=17
+
+    var area:Double=(sqrt(5*(5+2*sqrt(5)))*u*u)/4.0
+
+    println("Area : "+area)
+
+  }
+
+
+}
