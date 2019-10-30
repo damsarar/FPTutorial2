b889b026c4d2b11ceaf00266ec238f7f86f14939
chethani76 <chethaniwijesekara@gmail.com>
2019-10-30 21:02:11 +0530

    concatenate array

--- src/Demo.scala
+++ src/Demo.scala
@@ -1,9 +1,23 @@
 object Dem{
     def main(args:Array[String]){
+        //maps in scala
         val colors = Map("red"->"#FF0000", "azure"->"#F0FFFF", "peru"->"#CD853F")
         val nums: Mao[int, Int] = Map()
         println("keys in colors: "+ colors.keys)
         println("values in colors: "+colors.values)
         println("check if colors is empty : "+ colors.isEmpty)
+
+    //concat arrays
+
+        var arr1 = Array(1,2,3,4,5)
+        var arr2 = Array(8,9,10,11)
+
+        var arr3 = concat(arr1, arr2)
+
+        for(x<- arr3)
+        {
+            println(x)
+        }
     }
+
 }
