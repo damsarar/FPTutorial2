a72f5c0e552d69a4dd7c77897eb668a7be2f7ca2
chethani76 <chethaniwijesekara@gmail.com>
2019-10-30 20:55:24 +0530

    Maps in scala

---
+++ src/Demo.scala
@@ -0,0 +1,9 @@
+object Dem{
+    def main(args:Array[String]){
+        val colors = Map("red"->"#FF0000", "azure"->"#F0FFFF", "peru"->"#CD853F")
+        val nums: Mao[int, Int] = Map()
+        println("keys in colors: "+ colors.keys)
+        println("values in colors: "+colors.values)
+        println("check if colors is empty : "+ colors.isEmpty)
+    }
+}
